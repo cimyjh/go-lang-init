cf2631c4a5c383e655fcbf96dd35a83a0a035d55
cimyjh <cimyjh@gmail.com>
2020-07-12 20:36:35 +0900

    입력받은 한개의 파라미터에서 2개 이상의 output(결과물) 내기

--- main.go
+++ main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
-func multiply(a int, b int) int {
-	return a * b
+func lenAndUpper(name string) (int, string) {
+	return len(name), strings.ToUpper(name)
 }
 
 func main() {
-	fmt.Println(multiply(2, 2))
+	fmt.Println(lenAndUpper("hello world???!"))
 }
